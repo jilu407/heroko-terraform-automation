2fb23b52e3ebc6acf860cdcded9042f608f2babc
jilani <jilani.oracledba@gmail.com>
2020-09-04 10:57:03 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,3 +2,8 @@ provider "heroku" {
     email   = var.heroku_account_email
     api_key = var.heroku_api_key
   }
+resource "heroku_build" "this" {
+  app = heroku_app.this.id
+  source = {
+    path = "./node-js-getting-started"
+  }
